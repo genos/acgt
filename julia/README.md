1dcbdeec0019675ae6642c4377f3ef181382ebaf
Graham Enos <graham@strangeworks.com>
2024-05-23 16:07:04 -0400

    julia version

---
+++ julia/README.md
@@ -0,0 +1,7 @@
+Using [julia](https://julialang.org/).
+
+```
+julia --threads=auto acgt.jl
+  0.801761 seconds (1.39 M allocations: 649.561 MiB, 4.79% gc time, 12.14% compilation time)
+[149534612, 149517192, 149530828, 149536953]%
+```
---
+++ julia/acgt.jl
@@ -0,0 +1,17 @@
+import Distributed.@distributed
+
+_cs = [codepoint(x) for x in "ACGT"]
+
+function load(i::Int64)
+    x = zeros(Int64, 4)
+    s =  read(joinpath(@__DIR__, "..", "data", string(i) * ".acgt"))
+    for (i, c) in enumerate(_cs)
+        x[i] += count(==(c), s)
+    end
+    return x
+end
+
+@time x = @distributed ((a, b)->a+b) for i in 0:19_999
+    load(i)
+end
+print(x)
